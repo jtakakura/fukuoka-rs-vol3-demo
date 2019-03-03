5dee8a6e77a29a942a66a68848595981335fd8a7
Junji Takakura <j.takakura@gmail.com>
2019-03-03 17:50:52 +0900

    Impl simple window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+extern crate quicksilver;
+
+use quicksilver::{
+    geom::Vector,
+    graphics::Color,
+    lifecycle::{run, Settings, State, Window},
+    Result,
+};
+
+struct MainState;
+
+impl State for MainState {
+    fn new() -> Result<MainState> {
+        Ok(MainState {})
+    }
+
+    fn draw(&mut self, window: &mut Window) -> Result<()> {
+        window.clear(Color::WHITE)?;
+
+        Ok(())
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    run::<MainState>(
+        "Fukuoka.rs vol.3",
+        Vector::new(800, 600),
+        Settings::default(),
+    );
 }
